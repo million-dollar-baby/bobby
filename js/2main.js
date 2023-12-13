756457104508006950bdfe40e35aad1bd4daf19e
sbeyondt9 <73100240+sbeyondt9@users.noreply.github.com>
2023-12-13 18:05:42 +0530

    t
    t

---
+++ js/2main.js
@@ -0,0 +1,38 @@
+function initialize() {
+    $('#container').fadeOut("fast", function(){
+        $('#container').html('<section id="photos"></section>');
+        loadImages();
+    });
+    $('#container').fadeIn("slow");
+}
+
+function loadImages() {
+    var numOfImages = 100; // Change this to the number of images in the folder
+    var photosDiv = $('#photos');
+    
+    for (var i = numOfImages; i >= 1; i--) {
+        photosDiv.append('<div class="img-container"><img id="' + i + '" onmouseleave="hideInfo(' + i + ')" onmouseover="showInfo(' + i + ')" onmousedown="showImg(' + i + ')" src="2images/' + i + '.jpg" onError="this.remove();"/></div>');
+    }
+}
+
+function showInfo(id) {
+    var divID = '#' + id;
+    $(divID).animate({ opacity: 0.5 }, 100);
+}
+
+function hideInfo(id) {
+    var divID = '#' + id;
+    $(divID).animate({ opacity: 1 }, 300);
+}
+
+function showImg(id) {
+    $('#container').fadeOut("fast", function(){
+        $('#container').html('<img style="display: block; margin-left: auto; margin-right: auto;" src="2images/' + id + '.jpg" onmousedown="initialize()"/>');
+        $('#photos').css('display', 'none');
+    });
+    $('#container').fadeIn("slow");
+}
+
+$(document).ready(function() {
+    initialize();
+});
